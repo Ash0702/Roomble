d9c14095c35096c6d06e2df136b52f1dcb110ae4
Ash0702 <157949823+Ash0702@users.noreply.github.com>
2025-02-10 02:14:04 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,27 +4,26 @@ const dotenv = require('dotenv');
 dotenv.config();
 const path = require('node:path');
 const { Server } = require('socket.io');
-const mongoconnect = require('./mongodb');
-const app = express();
 const server = createServer(app);
 const io = new Server(server, {
     cors: {
         origin: '*',
     }
 });
+
+const Landlord_routes_auth = require(`./routes/Landlord_auth`)//contains Landlord authentication routing
+const Connectto_DB = require(`./database/database`)//contains module for connecting to database
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Connecting to Database
+Connectto_DB();
+
+
 app.use(express.json());
 
-const Landlord = require('./models/Landlord');
+app.use(`/authenticate`, Landlord_routes_auth);
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
 
-app.use('/api/auth',require('./routes/auth'));
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
-});
+app.listen(port, ()=>{console.log(`Server is now listening`)});
