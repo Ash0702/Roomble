befab22e86b085e67fc8047741553d21f794090d
BHUKYA VAISHNAVI <166046074+Vaishnavi0090@users.noreply.github.com>
2025-02-14 16:03:06 +0530

    Update OTP_models.js

--- backend/models/OTP_models.js
+++ backend/models/OTP_models.js
@@ -9,4 +9,20 @@ const LandlordSchema_OTP = new mongoose.Schema({
     createdat : {type : Date, default : Date.now, expires : 300}
 });
 
-module.exports = mongoose.model("LandLord_OTP", LandlordSchema_OTP);
+const TenantSchema_OTP = new mongoose.Schema({
+    name: {type: String, required: true, trim : true},
+    email: {type: String, required: true, trim : true, unique : true},
+    password: {type: String, required: true},
+    OTP : {type : String},
+    locality: { type: String, required: true},
+    gender: {type: String, required: true},
+    religion: {type: String, required: true},
+    alcohol: {type: Boolean, required: true },
+    veg: {type : Boolean, required: true },
+    pets: {type: Boolean, required: true},
+    createdat : {type : Date, default : Date.now, expires : 300}
+
+})
+
+module.exports ={LandlordSchema_OTP: mongoose.model("LandLord_OTP", LandlordSchema_OTP), 
+                 TenantScheman_OTP: mongoose.model("Tenant_OTP", TenantSchema_OTP};
